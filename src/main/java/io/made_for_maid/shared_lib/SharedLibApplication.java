051a6ef1b4a535c61f3d650a2ad41e501d29c51d
ish <tmdghks0615@gmail.com>
2025-04-13 23:11:14 +0900

    init

---
+++ src/main/java/io/made_for_maid/shared_lib/SharedLibApplication.java
@@ -0,0 +1,13 @@
+package io.made_for_maid.shared_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedLibApplication.class, args);
+	}
+
+}
