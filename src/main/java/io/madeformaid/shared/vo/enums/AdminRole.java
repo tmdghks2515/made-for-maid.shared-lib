ccedc88ed4deb7f2d40d6a3c2b7872873b5e2c89
ish <tmdghks0615@gmail.com>
2025-04-16 14:59:26 +0900

    관리자 역할 명 수정 및 매니저 역할 추가

--- src/main/java/io/madeformaid/shared/vo/enums/AdminRole.java
+++ src/main/java/io/madeformaid/shared/vo/enums/AdminRole.java
@@ -1,7 +1,8 @@
 package io.madeformaid.shared.vo.enums;
 
 public enum AdminRole implements DescribableEnum {
-    MAID_CAFE_ADMIN("메이드카페 관리자"),
+    MAID_CAFE_OWNER("메이드카페 사장님"),
+    MAID_CAFE_MANAGER("메이드카페 매니저"),
     MAID("메이드"),
     SYSTEM_ADMIN("시스템 관리자"),
     ;
--- src/main/java/io/madeformaid/shared/vo/enums/SignInResStatus.java
+++ src/main/java/io/madeformaid/shared/vo/enums/SignInResStatus.java
@@ -3,5 +3,6 @@ package io.madeformaid.shared.vo.enums;
 public enum SignInResStatus {
     SIGN_UP_SUCCESS,
     SIGN_IN_SUCCESS,
+    PROFILE_SELECT,
     OAUTH_FAILED,
 }
