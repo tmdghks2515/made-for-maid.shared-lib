5fd691c2499b05050bc61e41cea875a11b25ebc5
ish <tmdghks0615@gmail.com>
2025-04-17 18:10:25 +0900

    AuthProperties 추가

---
+++ src/main/java/io/madeformaid/shared/config/AuthProperties.java
@@ -0,0 +1,28 @@
+package io.madeformaid.shared.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "auth")
+public class AuthProperties {
+
+    private List<String> whitelist;
+    private String systemSecret;
+    private Jwt jwt = new Jwt();
+
+    @Getter
+    @Setter
+    public static class Jwt {
+        private String secret;
+        private String refreshTokenName;
+        private Long accessTokenExpireTime;
+        private Long refreshTokenExpireTime;
+    }
+}
